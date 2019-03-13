d458e4f8a758452be64a3ad835b166f9d223f4fc
Tuna205 <tuna454@gmail.com>
2019-03-13 23:39:39 +0100

    Add Vuforia Packets and nasic AR buisness card

---
+++ Assets/Scripts/MoveBlue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlue : MonoBehaviour
+{
+    public GameObject go;
+    public Material material;
+
+    public static MoveBlue instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null){
+            instance = this;
+        }
+        else Destroy(this);
+        //this.GetComponent<MeshRenderer>().material = material;
+        //go.GetComponent<MeshRenderer>().material = material;
+        
+    }
+
+    public void SetMaterial(){
+        this.GetComponent<MeshRenderer>().material = material;
+        go.GetComponent<MeshRenderer>().material = material;
+    }
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Material material = MoveBlue.instance.material;
+        this.GetComponent<MeshRenderer>().material = material;
+    }
+}
